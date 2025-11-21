b4ca97ce54c8ab0201dab887557cd03365660742
GvYoutube <gvyoutube@noreply.codeberg.org>
2025-11-21 06:20:43 +0100

    Update modemsong/index.js

--- modemsong/index.js
+++ modemsong/index.js
@@ -5,15 +5,16 @@ const animationContainer = document.getElementById('animation-container');
 
 const introFrames = [
     { time: 0, text: 'DIALING...' },
-        { time: 1500, text: 'DIALING...<br>ATDT 1-800-WEBTVSERVICE' },
-            { time: 3000, text: 'HANDSHAKING...' },
-                { time: 5000, text: 'HANDSHAKING...<br>PLEASE WAIT' },
-                    { time: 6000, text: 'CONNECTED!' },
-                    ];
+    { time: 1500, text: 'DIALING...<br>ATDT 1-800-WEBTVSERVICE' },
+    { time: 3000, text: 'HANDSHAKING...' },
+    { time: 5000, text: 'HANDSHAKING...<br>PLEASE WAIT' },
+    { time: 6000, text: 'CONNECTED!' },
+];
 
-                    const loopFrames = [
-                        { text: 'WELCOME TO THE WORLD WIDE WEB' },
-                            { text: `
+const loopFrames = [
+    { text: 'WELCOME TO THE WORLD WIDE WEB' },
+    {
+        text: `
                               *        *        *        *
                               *   *    *   *    *   *    *   *
                                * *      * *      * *      * *
